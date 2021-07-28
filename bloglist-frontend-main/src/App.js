94d0f1bb1b9dd5fa0b4943c3b72cdd7b287f927c
Nishant Tharani <nishant.tharani@gmail.com>
2021-07-29 00:20:51 +0100

    Work on part 5a and exercise 1

---
+++ bloglist-frontend-main/src/App.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+
+const App = () => {
+  const loginForm = () => {
+    return (
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({target}) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({target}) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    )
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      setUser(user)
+      blogService.setToken(user.token)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage("Invalid Login Credentials")
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);  // the entire user object
+  const [errorMessage, setErrorMessage] = useState(null)
+
+
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      {
+        setBlogs(blogs)
+      }
+    )
+  }, [user])
+
+  return (
+    <div>
+      <h2>blogs</h2>
+
+      {user === null ?
+        <div>
+          <p>{errorMessage}</p>
+          {loginForm()}
+        </div>
+        :
+        <div>
+          {user.name} is logged in
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog}/>
+          )}
+        </div>
+      }
+    </div>
+  )
+}
+
+export default App
